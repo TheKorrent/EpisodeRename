c6e0e633173b1c846b94641c8819b30b72e9145b
ShizukiNatsuki <shizuki.natsuki@outlook.com>
2026-01-17 10:07:47 +0800

    refactor: improve default null config manager

--- src/main/kotlin/io/github/thekorrent/episode/rename/EpisodeRenamePlugin.kt
+++ src/main/kotlin/io/github/thekorrent/episode/rename/EpisodeRenamePlugin.kt
@@ -1,14 +1,14 @@
 package io.github.thekorrent.episode.rename
 
+import moe.shizuki.korrent.defaultNullPluginConfigManager
 import moe.shizuki.korrent.plugin.KorrentPlugin
-import moe.shizuki.korrent.plugin.config.PluginConfigManager
 import org.pf4j.PluginWrapper
 
 class EpisodeRenamePlugin(wrapper: PluginWrapper) : KorrentPlugin(wrapper) {
     companion object {
         val config get() = _config
 
-        private var _pluginConfigManager = PluginConfigManager("null")
+        private var _pluginConfigManager = defaultNullPluginConfigManager
         private var _config = EpisodeRenameConfig()
     }
 
