fd448194da3e92187b2217f595805421f14a1e48
Shizuki Natsuki <shizuki.natsuki@outlook.com>
2026-01-17 17:43:07 +0800

    feat: offset support (#5)

--- src/main/kotlin/io/github/thekorrent/episode/rename/EpisodeRenamePlugin.kt
+++ src/main/kotlin/io/github/thekorrent/episode/rename/EpisodeRenamePlugin.kt
@@ -1,17 +1,33 @@
 package io.github.thekorrent.episode.rename
 
 import moe.shizuki.korrent.defaultNullPluginConfigManager
+import moe.shizuki.korrent.defaultNullPluginDataManager
+import moe.shizuki.korrent.objectMapper
 import moe.shizuki.korrent.plugin.KorrentPlugin
 import org.pf4j.PluginWrapper
+import java.io.File
 
 class EpisodeRenamePlugin(wrapper: PluginWrapper) : KorrentPlugin(wrapper) {
     companion object {
+        val pluginDataManager get() = _pluginDataManager
         val config: EpisodeRenameConfig by lazy { _pluginConfigManager.load() }
 
         private var _pluginConfigManager = defaultNullPluginConfigManager
+        private var _pluginDataManager = defaultNullPluginDataManager
     }
 
     init {
         _pluginConfigManager = this.pluginConfigManager
+        _pluginDataManager = this.pluginDataManager
+    }
+
+    override fun start() {
+        val offsets = File(pluginDataManager.pluginDataFolder, "offsets.json")
+
+        if(!offsets.exists()) {
+            objectMapper.writeValue(offsets, mapOf("category" to 0))
+        }
+
+        super.start()
     }
 }
